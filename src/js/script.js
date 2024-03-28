531a5289041572d48b2aaae3709c594c55064df3
Lafoerte91 <lafoerte@gmail.com>
2024-03-28 14:21:18 +0300

    PHP Mailer

--- src/js/script.js
+++ src/js/script.js
@@ -101,4 +101,23 @@ $(document).ready(function(){
 
   // Mask
   $('input[name=phone]').mask("+7 (999) 999-9999")
+
+  // Ajax
+  $('form').submit(function(e) {
+    e.preventDefault();
+    if(!$(this).valid()) {
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
+    })
+    return false;
+  })
 })
